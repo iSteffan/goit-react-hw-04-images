36615c484771f9cc4758531b5e213e0acaa3a199
Stepan Skichko <st.skichko@gmail.com>
2023-04-28 18:02:32 +0300

    searchbar DONE

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,39 +1,36 @@
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { Field, Form, Header, Btn, BtnLabel } from './Searchbar.styled';
 
 const FormSchema = Yup.object().shape({
   keyword: Yup.string().required('Required field!'),
 });
 
-export class Searchbar extends Component {
-  render() {
-    return (
-      <Header>
-        <Formik
-          initialValues={{
-            keyword: '',
-          }}
-          validationSchema={FormSchema}
-          onSubmit={(values, actions) => {
-            this.props.onSubmit({ ...values });
-            actions.resetForm();
-          }}
-        >
-          <Form>
-            <Btn type="submit">
-              <BtnLabel>Search</BtnLabel>
-            </Btn>
+export const Searchbar = ({ onSubmit }) => {
+  return (
+    <Header>
+      <Formik
+        initialValues={{
+          keyword: '',
+        }}
+        validationSchema={FormSchema}
+        onSubmit={(values, actions) => {
+          onSubmit({ ...values });
+          actions.resetForm();
+        }}
+      >
+        <Form>
+          <Btn type="submit">
+            <BtnLabel>Search</BtnLabel>
+          </Btn>
 
-            <Field name="keyword" placeholder="Search images and photos" />
-          </Form>
-        </Formik>
-      </Header>
-    );
-  }
-}
+          <Field name="keyword" placeholder="Search images and photos" />
+        </Form>
+      </Formik>
+    </Header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
