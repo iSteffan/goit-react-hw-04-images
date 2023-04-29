6550c5d2c8a48e9ce82b0c461b9ef8ff003d70fc
Stepan Skichko <st.skichko@gmail.com>
2023-04-29 11:58:14 +0300

    component ImageGalleryItem hooks DONE

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,54 +1,43 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ListItem, Image } from './ImageGalleryItem.styled';
 import { Modal } from 'components/Modal/Modal';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalShown: false,
-    modalImg: '',
-    modalAlt: '',
-  };
+export const ImageGalleryItem = ({ images }) => {
+  const [isModalShown, setIsModalShown] = useState(false);
+  const [modalImg, setModalImg] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
 
   // При натисненні на картинку зберігаємо її дані для модалки
-  onSave = (img, alt) => {
-    this.setState({
-      isModalShown: true,
-      modalImg: img,
-      modalAlt: alt,
-    });
+  const onSave = (img, alt) => {
+    setIsModalShown(true);
+    setModalImg(img);
+    setModalAlt(alt);
   };
 
   // При натисненні виставляєм прапорець не показувати модалку
-  onClose = () => {
-    this.setState({
-      isModalShown: false,
-    });
+  const onClose = () => {
+    setIsModalShown(false);
   };
 
-  render() {
-    const { images } = this.props;
-    const { isModalShown, modalImg, modalAlt } = this.state;
-
-    return (
-      <>
-        <ListItem>
-          <Image
-            loading="lazy"
-            src={images.webformatURL}
-            alt={images.tags}
-            onClick={() => {
-              this.onSave(images.largeImageURL, images.tags);
-            }}
-          />
-          {isModalShown && (
-            <Modal alt={modalAlt} img={modalImg} onClose={this.onClose} />
-          )}
-        </ListItem>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ListItem>
+        <Image
+          loading="lazy"
+          src={images.webformatURL}
+          alt={images.tags}
+          onClick={() => {
+            onSave(images.largeImageURL, images.tags);
+          }}
+        />
+        {isModalShown && (
+          <Modal alt={modalAlt} img={modalImg} onClose={onClose} />
+        )}
+      </ListItem>
+    </>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   images: PropTypes.object,
