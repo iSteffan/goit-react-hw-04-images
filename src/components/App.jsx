8243ecb31fe23d7616ff26cbba5bf78aa6a1b4ce
Stepan Skichko <st.skichko@gmail.com>
2023-04-29 12:30:04 +0300

    task DONE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { getImages } from '../services';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -6,96 +6,74 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { ErrorMessage } from './ImageGallery/ImageGallery.styled';
 
-export class App extends Component {
-  state = {
-    keyword: '',
-    images: [],
-    page: 1,
-    loading: false,
-    total: 1,
-    findByKeyword: true,
-  };
+export const App = () => {
+  const [keyword, setKeyword] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState(1);
+  const [findByKeyword, setFindByKeyword] = useState(true);
 
   // Отримуємо доступ до ключового слова / скидаємо прапорці в початковий стан
-  searchByKeyword = key => {
+  const searchByKeyword = key => {
     const keywordValue = Object.values(key);
-    this.setState({
-      keyword: keywordValue,
-      page: 1,
-      loading: false,
-      total: 1,
-      findByKeyword: true,
-    });
+    setKeyword(keywordValue);
+    setPage(1);
+    setLoading(false);
+    setTotal(1);
+    setFindByKeyword(true);
   };
 
   // При кліку на кнопку змінюємо сторінку для пошуку
-  onLoadMoreClick = () => {
-    this.setState(prevSt => ({
-      page: prevSt.page + 1,
-    }));
+  const onLoadMoreClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  setImages = (keyword, page) => {
+  useEffect(() => {
+    if (!keyword) {
+      return;
+    }
+
     getImages(keyword, page)
       .then(resp => resp.json())
       .then(response => {
         // console.log(response);
         // якщо нічого не знайдено - скидаємо всі прапорці та масив зображень
         if (response.total === 0) {
-          this.setState({
-            images: [],
-            loading: false,
-            findByKeyword: false,
-          });
+          setImages([]);
+          setLoading(false);
+          setFindByKeyword(false);
         }
         // Якщо отриманий масив не порожній то перезаписуємо масив зображень/вимикаємо спінер/
         // встановлюєм прапорець findByKeyword/ записуємо заг кількість результатів
         else {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...response.hits],
-            loading: false,
-            findByKeyword: true,
-            total: response.total,
-          }));
+          setImages(prevImages => [...prevImages, ...response.hits]);
+          setLoading(false);
+          setFindByKeyword(true);
+          setTotal(response.total);
         }
       })
       .catch(error => console.error(error));
-  };
+  }, [keyword, page]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.keyword !== this.state.keyword ||
-      prevState.page !== this.state.page
-    ) {
-      prevState.keyword !== this.state.keyword
-        ? this.setState({ loading: true, images: [] })
-        : this.setState({ loading: true });
+  return (
+    <div>
+      <Searchbar onSubmit={searchByKeyword} />
 
-      this.setImages(this.state.keyword, this.state.page);
-    }
-  }
+      {/* Перевіряємо чи знайшли фото по запиту - показуємо галерею/повідомлення про невдалий пошук */}
+      {findByKeyword ? (
+        <ImageGallery images={images} />
+      ) : (
+        <ErrorMessage>
+          Sorry, we can't find photo by tag "{keyword}"
+        </ErrorMessage>
+      )}
 
-  render() {
-    const { images, keyword, loading, total, findByKeyword, page } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.searchByKeyword} />
+      {/* Перевіряємо стан loading та показуємо спінер*/}
+      {loading && <Loader />}
 
-        {/* Перевіряємо чи знайшли фото по запиту - показуємо галерею/повідомлення про невдалий пошук */}
-        {findByKeyword ? (
-          <ImageGallery images={images} />
-        ) : (
-          <ErrorMessage>
-            Sorry, we can't find photo by tag "{keyword}"
-          </ErrorMessage>
-        )}
-
-        {/* Перевіряємо стан loading та показуємо спінер*/}
-        {loading && <Loader />}
-
-        {/* Перевіряємо кількість сторінок та показуємо кнопку "завантажити ще" */}
-        {total / 12 > page && <Button onClick={this.onLoadMoreClick} />}
-      </div>
-    );
-  }
-}
+      {/* Перевіряємо кількість сторінок та показуємо кнопку "завантажити ще" */}
+      {total / 12 > page && <Button onClick={onLoadMoreClick} />}
+    </div>
+  );
+};
