5c6309c2686f492e9aea2b88a3362f472312eb96
Stepan Skichko <st.skichko@gmail.com>
2023-04-29 12:48:25 +0300

    delete icon

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -51,7 +51,7 @@ export const Btn = styled.button`
   width: 48px;
   height: 48px;
   border: 0;
-  background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+
   background-size: 40%;
   background-repeat: no-repeat;
   background-position: center;
